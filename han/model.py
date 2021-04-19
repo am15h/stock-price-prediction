f1b40f5af0be5d47b642a3aa1c383693af7faaf0
am15h <agarg@cs.iitr.ac.in>
2021-04-19 19:33:12 +0530

    stocknet with prices vector

--- han/model.py
+++ han/model.py
@@ -1,97 +1,119 @@
 import tensorflow as tf
 import numpy as np
-
+from numba import jit
 from tensorflow.keras.layers import Embedding, Dropout, Dense, GRU, CuDNNGRU, Bidirectional
 import tensorflow.nn as nn
 
 
 class HAN(tf.keras.Model):
-    def __init__(self, wordvec, params):
-        
-        super(HAN, self).__init__()
-        self.params = params
+	def __init__(self, wordvec, params):
 
-        # wordvec: ndarray
-        vocab_size = wordvec.shape[0]
-        self.embedding_dim = wordvec.shape[1]
-        self.embedding =  Embedding(vocab_size, self.embedding_dim, weights=[wordvec],mask_zero=True,trainable=True,)
+		super(HAN, self).__init__()
+		self.params = params
 
-        self.dropout =  Dropout(rate=self.params['dr'])  # StockNet
+		# wordvec: ndarray
+		vocab_size = wordvec.shape[0]
+		self.embedding_dim = wordvec.shape[1]
+		self.embedding = Embedding(vocab_size, self.embedding_dim, weights=[wordvec], mask_zero=True, trainable=True, )
 
-        # Word-level attention
-        self.word_att =  Dense(1, activation=nn.sigmoid)
+		self.dropout = Dropout(rate=self.params['dr'])  # StockNet
 
-        # News-level attention
-        self.news_att =  Dense(1, activation=nn.sigmoid)
+		# Word-level attention
+		self.word_att = Dense(1, activation=nn.sigmoid)
 
-        # Sequence modeling
-        self.bi_gru = self.make_bi_gru(self.embedding_dim)
+		# News-level attention
+		self.news_att = Dense(1, activation=nn.sigmoid)
 
-        # Temporal attention
-        self.temp_att =  Dense(1, activation=nn.sigmoid)
+		# Sequence modeling
+		self.bi_gru = self.make_bi_gru(self.embedding_dim)
 
-        # Discriminative Network (MLP)
-        self.fc0 =  Dense(self.params['hidden_size'], activation = nn.elu)
-        self.fc1 =  Dense(self.params['hidden_size'], activation = nn.elu)
+		# Temporal attention
+		self.temp_att = Dense(1, activation=nn.sigmoid)
 
-        self.fc_out =  Dense(2)
+		# Discriminative Network (MLP)
+		self.fc0 = Dense(self.params['hidden_size'], activation=nn.elu)
+		self.fc1 = Dense(self.params['hidden_size'], activation=nn.elu)
 
-    def call(self, x, day_len, news_len, training=False):
+		self.fc_out = Dense(2)
 
-        max_dlen = tf.keras.backend.max(day_len).numpy()
-        max_nlen = tf.keras.backend.max(news_len).numpy()
-        
-        max_dlen = np.max(day_len)
-        max_nlen = np.max(news_len)
+	def call(self, x, params, day_len, news_len, training=False):
 
-        x = x[:, :, :max_dlen, :max_nlen]
-        news_len = news_len[:, :, :max_dlen]
+		max_dlen = tf.keras.backend.max(day_len).numpy()
+		max_nlen = tf.keras.backend.max(news_len).numpy()
 
-        # Averaged daily news corpus
-        # (batch_size, days, max_daily_news, max_news_words
-        # -> (batch_size, days, max_daily_news, max_news_words, embedding_dim)
-        x = self.embedding(x)
+		max_dlen = np.max(day_len)
+		max_nlen = np.max(news_len)
 
-        # handle variable-length news word sequences
-        mask = tf.sequence_mask(news_len, maxlen=max_nlen, dtype=tf.float32)
-        mask = tf.expand_dims(mask, axis=4)
-        x *= mask
+		# print("RAW: ", 'X:', x.shape, ' P:', params.shape)
 
-        # Word-level attention
-        # x: (batch_size, days, max_daily_news, max_news_words, embedding_dim)
-        # t: (batch_size, days, max_daily_news, max_news_words, 1)
-        # n: (batch_size, days, max_daily_news, embedding_dim)
-        word_att = self.word_att(x)
-        n = nn.softmax(word_att, axis=3) * x
-        n = tf.reduce_sum(n, axis=3)
+		x = x[:, :, :max_dlen, :max_nlen]
+		params = params[:, :, :max_dlen, :]
+		news_len = news_len[:, :, :max_dlen]
 
-        # handle variable-length day news sequences
-        mask = tf.sequence_mask(day_len, maxlen=max_dlen, dtype=tf.float32)
-        mask = tf.expand_dims(mask, axis=3)
-        n *= mask
+		# print("Initial: ", 'X:', x.shape, ' P:', params.shape, ' N:', news_len.shape)
 
-        # News-level attention
-        news_att = self.news_att(n)
-        d = nn.softmax(news_att, axis=2) * n
-        d = tf.reduce_sum(d, axis=2)
+		# Averaged daily news corpus
+		# (batch_size, days, max_daily_news, max_news_words + 7)
+		# -> (batch_size, days, max_daily_news, max_news_words, embedding_dim)
+		x = self.embedding(x)
 
-        # Sequence modeling
-        gru = self.bi_gru(d, training=training)
+		# print("After embedding: ", 'X: ', x.shape)
 
-        # Temporal attention
-        temp_att = self.temp_att(gru)
-        v = nn.softmax(temp_att, axis=2) * gru
-        v = tf.reduce_sum(v, axis=1)
+		# handle variable-length news word sequences
+		mask = tf.sequence_mask(news_len, maxlen=max_nlen, dtype=tf.float32)
+		mask = tf.expand_dims(mask, axis=4)
+		x *= mask
 
-        # Discriminative Network (MLP)
-        v = self.fc0(v)
-        v = self.dropout(v) if training else v
-        v = self.fc1(v)
-        v = self.dropout(v) if training else v
-        return self.fc_out(v)
+		# print("After mask: ", 'X: ', x.shape)
 
-    def make_bi_gru(self, units):
-        if tf.test.is_gpu_available() and not self.params['no_gpu']:
-            return  Bidirectional(CuDNNGRU(units, return_sequences=True), merge_mode='concat')
-        else:
-            return  Bidirectional(GRU(units, return_sequences=True), merge_mode='concat')
+		# Word-level attention
+		# x: (batch_size, days, max_daily_news, max_news_words, embedding_dim)
+		# t: (batch_size, days, max_daily_news, max_news_words, 1)
+		# n: (batch_size, days, max_daily_news, embedding_dim)
+		word_att = self.word_att(x)
+		n = nn.softmax(word_att, axis=3) * x
+		n = tf.reduce_sum(n, axis=3)
+
+		# print("After word_attn N: ", 'N: ', n.shape)
+
+		# handle variable-length day news sequences
+		mask = tf.sequence_mask(day_len, maxlen=max_dlen, dtype=tf.float32)
+		mask = tf.expand_dims(mask, axis=3)
+		n *= mask
+
+		# print("After mask N: ", 'N: ', n.shape)
+
+		tf.cast(n, dtype=tf.float32)
+
+		# print('N: val', n[0][0][0][0], 'P: val', params[0][0][0][0])
+
+		n_params = tf.concat([n, params], axis=3)
+
+		# print('(2) N: val', n[0][0][0][0], 'P: val', params[0][0][0][0])
+
+		# News-level attention
+		news_att = self.news_att(n_params)
+		d = nn.softmax(news_att, axis=2) * n_params
+		d = tf.reduce_sum(d, axis=2)
+
+		# Sequence modeling
+		gru = self.bi_gru(d, training=training)
+
+		# Temporal attention
+		temp_att = self.temp_att(gru)
+		v = nn.softmax(temp_att, axis=2) * gru
+		v = tf.reduce_sum(v, axis=1)
+
+		# Discriminative Network (MLP)
+		v = self.fc0(v)
+		v = self.dropout(v) if training else v
+		v = self.fc1(v)
+		v = self.dropout(v) if training else v
+		return self.fc_out(v)
+
+	def make_bi_gru(self, units):
+		if tf.test.is_gpu_available() and not self.params['no_gpu']:
+			return Bidirectional(CuDNNGRU(units, return_sequences=True), merge_mode='concat')
+		else:
+			return Bidirectional(GRU(units, return_sequences=True), merge_mode='concat')
+
--- han/train.py
+++ han/train.py
@@ -20,116 +20,117 @@ tfe = tf.contrib.eager
 
 
 def loss(logits, labels, weights):
-    weighted_labels = tf.reduce_sum(
-        tf.constant(weights, dtype=tf.float32) * tf.one_hot(labels, 2), axis=1)
-    unweighted_losses = tf.nn.sparse_softmax_cross_entropy_with_logits(
-        labels=labels, logits=logits)
-    return tf.reduce_mean(unweighted_losses * weighted_labels)
+	weighted_labels = tf.reduce_sum(
+		tf.constant(weights, dtype=tf.float32) * tf.one_hot(labels, 2), axis=1)
+	unweighted_losses = tf.nn.sparse_softmax_cross_entropy_with_logits(
+		labels=labels, logits=logits)
+	return tf.reduce_mean(unweighted_losses * weighted_labels)
 
 
 def compute_accuracy(logits, labels):
-    predictions = tf.argmax(logits, axis=1, output_type=tf.int64)
-    labels = tf.cast(labels, tf.int64)
-    batch_size = int(logits.shape[0])
-    return tf.reduce_sum(
-        tf.cast(tf.equal(predictions, labels), dtype=tf.float32)) / batch_size
+	predictions = tf.argmax(logits, axis=1, output_type=tf.int64)
+	labels = tf.cast(labels, tf.int64)
+	batch_size = int(logits.shape[0])
+	return tf.reduce_sum(
+		tf.cast(tf.equal(predictions, labels), dtype=tf.float32)) / batch_size
 
 
 def train_step(model, optimizer, dataset, step_counter, ep, class_weights, params, log_interval=None):
 
-    start = time.time()
-    steps = 0
-    total_loss = 0
-    for step, ((days, day_lens, news_lens), labels) in enumerate(dataset):
-        steps += 1
-        with tf.GradientTape() as tape:
-            logits = model(days, day_lens, news_lens, training=True)
-            loss_value = loss(logits, labels, class_weights)
-            total_loss += loss_value
+	start = time.time()
+	steps = 0
+	total_loss = 0
+	# params
+	for step, ((days, prices, day_lens, news_lens), labels) in enumerate(dataset):
+		steps += 1
+		with tf.GradientTape() as tape:
+			logits = model(days, prices, day_lens, news_lens, training=True)
+			loss_value = loss(logits, labels, class_weights)
+			total_loss += loss_value
 
-        grads = tape.gradient(loss_value, model.trainable_weights)
-        grads, _ = clip_ops.clip_by_global_norm(grads, params['clip_norm'])
+		grads = tape.gradient(loss_value, model.trainable_weights)
+		grads, _ = clip_ops.clip_by_global_norm(grads, params['clip_norm'])
 
-        optimizer.apply_gradients(zip(grads, model.trainable_weights), global_step=step_counter)
+		optimizer.apply_gradients(zip(grads, model.trainable_weights), global_step=step_counter)
 
-    params['train_losses'].append(total_loss/steps)
+	params['train_losses'].append(total_loss/steps)
 
 
 def test(model, dataset, class_weights, show_classification_report=False, ds_name='Test'):
 
-    start = time.time()
-    avg_loss = tfe.metrics.Mean('loss', dtype=tf.float32)
-    accuracy = tfe.metrics.Accuracy('accuracy', dtype=tf.float32)
+	start = time.time()
+	avg_loss = tfe.metrics.Mean('loss', dtype=tf.float32)
+	accuracy = tfe.metrics.Accuracy('accuracy', dtype=tf.float32)
 
-    y_true = list()
-    y_pred = list()
-    for (days, day_lens, news_lens), labels in dataset:
-        logits = model(days, day_lens, news_lens, training=False)
-        avg_loss(loss(logits, labels, class_weights))
-        pred = tf.argmax(logits, axis=1, output_type=tf.int64)
-        accuracy(pred, tf.cast(labels, tf.int64))
+	y_true = list()
+	y_pred = list()
+	for (days, prices, day_lens, news_lens), labels in dataset:
+		logits = model(days, prices, day_lens, news_lens, training=False)
+		avg_loss(loss(logits, labels, class_weights))
+		pred = tf.argmax(logits, axis=1, output_type=tf.int64)
+		accuracy(pred, tf.cast(labels, tf.int64))
 
-        if show_classification_report:
-            y_true.extend(labels.numpy().tolist())
-            y_pred.extend(pred.numpy().tolist())
-    end = time.time()
-    print('%s set: Average loss: %.6f, Accuracy: %.3f%% (%.3f sec)' %
-          (ds_name, avg_loss.result(), 100 * accuracy.result(), end - start))
+		if show_classification_report:
+			y_true.extend(labels.numpy().tolist())
+			y_pred.extend(pred.numpy().tolist())
+	end = time.time()
+	print('%s set: Average loss: %.6f, Accuracy: %.3f%% (%.3f sec)' %
+		  (ds_name, avg_loss.result(), 100 * accuracy.result(), end - start))
 
 
-    if show_classification_report:
-        print(classification_report(y_true, y_pred, target_names=['DOWN', 'UP']))
+	if show_classification_report:
+		print(classification_report(y_true, y_pred, target_names=['DOWN', 'UP']))
 
-    return accuracy.result(), avg_loss.result()
+	return accuracy.result(), avg_loss.result()
 
 
 def train(parameters, train_ds, val_ds, wordvec, class_weights):
-    tf.enable_eager_execution()
-    tf.logging.set_verbosity(tf.logging.ERROR)
+	tf.enable_eager_execution()
+	tf.logging.set_verbosity(tf.logging.ERROR)
 
-    random_seed.set_random_seed(parameters['seed'])
+	random_seed.set_random_seed(parameters['seed'])
 
-    (device, data_format) = ('/gpu:0', 'channels_first')
-    if parameters['no_gpu'] > 0 or not tf.test.is_gpu_available():
-        (device, data_format) = ('/cpu:0', 'channels_last')
-    print('Using device %s, and data format %s.' % (device, data_format))
+	(device, data_format) = ('/gpu:0', 'channels_first')
+	if parameters['no_gpu'] > 0 or not tf.test.is_gpu_available():
+		(device, data_format) = ('/cpu:0', 'channels_last')
+	print('Using device %s, and data format %s.' % (device, data_format))
 
-    model = HAN(wordvec, parameters)
-    
-    optimizer = AdamWeightDecayOptimizer(
-        learning_rate=parameters['learning_rate'],
-        weight_decay_rate=0.0,
-        beta_1=0.9,
-        beta_2=0.999,
-        epsilon=1e-6,
-        exclude_from_weight_decay=["LayerNorm", "layer_norm", "bias"])
+	model = HAN(wordvec, parameters)
 
-    timestamp = datetime.now().strftime(' %d%m%y %H%M%S')
-    
-    # Create and restore checkpoint (if one exists on the path)
-    checkpoint_prefix = os.path.join(parameters['model_dir'], 'ckpt')
-    step_counter = tf.train.get_or_create_global_step()
-    checkpoint = tf.train.Checkpoint(model=model, optimizer=optimizer, step_counter=step_counter)
+	optimizer = AdamWeightDecayOptimizer(
+		learning_rate=parameters['learning_rate'],
+		weight_decay_rate=0.0,
+		beta_1=0.9,
+		beta_2=0.999,
+		epsilon=1e-6,
+		exclude_from_weight_decay=["LayerNorm", "layer_norm", "bias"])
 
-    best_acc_ep = (0.0, -1, float('inf'))  # acc, epoch, loss
-    patience = 0
+	timestamp = datetime.now().strftime(' %d%m%y %H%M%S')
 
-    with tf.device(device):
-        for ep in range(parameters['train_epochs']):
-            start = time.time()
-            train_step(model, optimizer, train_ds, step_counter, ep, class_weights, parameters, parameters['log_interval'])
-            
-            val_acc, val_loss = test(model, val_ds, class_weights, ds_name='Val')
+	# Create and restore checkpoint (if one exists on the path)
+	checkpoint_prefix = os.path.join(parameters['model_dir'], 'ckpt')
+	step_counter = tf.train.get_or_create_global_step()
+	checkpoint = tf.train.Checkpoint(model=model, optimizer=optimizer, step_counter=step_counter)
 
-            end = time.time()
-            print('\n Epoch: {} \tTime: {:.6f}'.format(ep + 1, end - start))
+	best_acc_ep = (0.0, -1, float('inf'))  # acc, epoch, loss
+	patience = 0
 
-            parameters['val_losses'].append(val_loss)
+	with tf.device(device):
+		for ep in range(parameters['train_epochs']):
+			start = time.time()
+			train_step(model, optimizer, train_ds, step_counter, ep, class_weights, parameters, parameters['log_interval'])
 
-            if val_loss.numpy() < best_acc_ep[2]:
-                best_acc_ep = (val_acc.numpy(), ep, val_loss.numpy())
-                print('Save checkpoint', checkpoint_prefix)
-                checkpoint.save(checkpoint_prefix)
+			val_acc, val_loss = test(model, val_ds, class_weights, ds_name='Val')
+
+			end = time.time()
+			print('\n Epoch: {} \tTime: {:.6f}'.format(ep + 1, end - start))
+
+			parameters['val_losses'].append(val_loss)
+
+			if val_loss.numpy() < best_acc_ep[2]:
+				best_acc_ep = (val_acc.numpy(), ep, val_loss.numpy())
+				print('Save checkpoint', checkpoint_prefix)
+				checkpoint.save(checkpoint_prefix)
 #            else:
 #                if patience == parameters['patience']:
 #                    print('Apply early stopping')
@@ -138,52 +139,52 @@ def train(parameters, train_ds, val_ds, wordvec, class_weights):
 #                patience += 1
 #                print('patience {}/{}'.format(patience, parameters['patience']))
 
-        print('Min loss {:.6f}, dev acc. {:.3f}%, ep {} \n'.format(
-                best_acc_ep[2], best_acc_ep[0] * 100., best_acc_ep[1] + 1))
-        
-
-    model._name = "Hybrid Attention Network"
-    model.summary()
+		print('Min loss {:.6f}, dev acc. {:.3f}%, ep {} \n'.format(
+				best_acc_ep[2], best_acc_ep[0] * 100., best_acc_ep[1] + 1))
 
 
-    plt.ylabel('Training/Validation Loss')
-    plt.xlabel('Number of Epochs')
-    plt.plot(parameters['train_losses'], label="Train Loss")
-    plt.plot(parameters['val_losses'], label="Validation Loss")
-    plt.legend()
-    plt.show();
-    plt.savefig('han_training_curve.png')
-    plt.gcf().clear()
+	model._name = "Hybrid Attention Network"
+	model.summary()
+
+
+	plt.ylabel('Training/Validation Loss')
+	plt.xlabel('Number of Epochs')
+	plt.plot(parameters['train_losses'], label="Train Loss")
+	plt.plot(parameters['val_losses'], label="Validation Loss")
+	plt.legend()
+	plt.show();
+	plt.savefig('han_training_curve.png')
+	plt.gcf().clear()
 
 """
 if __name__ == '__main__':
-    
-    params = {}
-    params['optimizer'] = 'adam'
-    params['decay_rate'] = 0.0
-    params['batch_size'] = 32
-    params['learning_rate'] = 1e-3
-    params['loss_function'] = 'mean_squared_error'
-    params['betas'] = [0.9, 0.999]
-    params['dr'] = 0.3
-    params['l2'] = 1e-6
-    params['clip_norm'] = 5.0
-    params['hidden_size'] = 50
-    params['train_epochs'] = 2
-    params['patience'] = 1
-    params['log_interval'] = 50
-    params['vocab_size'] = 33000
-    params['days'] = 5
-    params['max_date_len'] = 40
-    params['max_news_len'] = 30
-    params['seed'] = 2019
-    params['data_path'] = 'data/sp500glove.pkl'
-    params['no_gpu'] = 0
-    params['output_dir'] = 'summaries/'
-    params['model_dir'] = 'checkpoints'
-    params['train_losses'] = []
-    params['val_losses'] = []
 
-    train(params)
+	params = {}
+	params['optimizer'] = 'adam'
+	params['decay_rate'] = 0.0
+	params['batch_size'] = 32
+	params['learning_rate'] = 1e-3
+	params['loss_function'] = 'mean_squared_error'
+	params['betas'] = [0.9, 0.999]
+	params['dr'] = 0.3
+	params['l2'] = 1e-6
+	params['clip_norm'] = 5.0
+	params['hidden_size'] = 50
+	params['train_epochs'] = 2
+	params['patience'] = 1
+	params['log_interval'] = 50
+	params['vocab_size'] = 33000
+	params['days'] = 5
+	params['max_date_len'] = 40
+	params['max_news_len'] = 30
+	params['seed'] = 2019
+	params['data_path'] = 'data/sp500glove.pkl'
+	params['no_gpu'] = 0
+	params['output_dir'] = 'summaries/'
+	params['model_dir'] = 'checkpoints'
+	params['train_losses'] = []
+	params['val_losses'] = []
+
+	train(params)
 
 """
